76a96fb1a64fbfd535ac03bcbc2b6834391b8d04
Cat <ccallcottstevens@gmail.com>
2021-07-06 16:15:23 +1000

    login page recreated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { firebase, FieldValue } from './lib/firebase'
+import FirebaseContext from './context/firebase'
 import './index.css';
 import App from './App';
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+  <App />
+</FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
---
+++ src/pages/signup.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function signup() {
+    return(
+        <h1>I am the Signup</h1>
+    )
+}
